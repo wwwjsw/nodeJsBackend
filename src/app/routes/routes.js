1504d214287c5ed0d74f87d3f3a72dab8a71bd96
Guilherme Henrique <progr.guilherme@gmail.com>
2019-06-06 21:49:59 -0300

    update start script and, marko configuration

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,9 +1,25 @@
+const list = require('../views/lists/list/list.marko');
+
 module.exports = (app) => {
     app.get('/', function(req, resp) {
         resp.send('ok, but nothing here :/');
     });
     
     app.get('/list', function(req, resp) {
-        resp.send('list :)');
+        resp.marko(
+            list,
+            {
+                items : [
+                    {
+                        id : 1,
+                        name : 'item1',
+                    },
+                    {
+                        id : 2,
+                        name : 'item2',
+                    }
+                ]
+            }
+        );
     });
 }
