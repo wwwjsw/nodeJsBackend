cd37f7951a0c6543703c66433f1c76130a7bde0b
guilherme henrique <progr.guilherme@gmail.com>
2019-08-28 00:51:51 -0300

    new method add() + bodyParser middleware

--- src/app/infra/item-dao.js
+++ src/app/infra/item-dao.js
@@ -1,20 +1,44 @@
 class ItemDao {
     constructor(db) {
-        this._db = db;
+        this._db = db
     }
 
     list() {
         return new Promise((resolve, reject) => {
-            this._db.all(
-                'SELECT * FROM items',
+            this._db.all(`SELECT * FROM items`,
                 (err, result) => {
-                    if(err) return reject(`could't get any item`);
+                    if(err) return reject(`could't get any item`)
 
-                    return resolve(result);
+                    return resolve(result)
                 }
             )
-        });
+        })
     }
+
+    add(item) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO items (
+                    name,
+                    price,
+                    desc
+                ) values (?, ?, ?)`,
+                [
+                    item.name,
+                    item.price,
+                    item.desc
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err)
+                        return reject('reject item')
+                    }
+    
+                    resolve()
+                }
+            )
+        })
+    } 
 }
 
-module.exports = ItemDao;
+module.exports = ItemDao
