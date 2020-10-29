1e6c596397dda6d4e234d50432fbeeda16ea4c7d
Guilherme henrique machado <progr.guilherme@gmail.com>
2020-10-28 23:29:03 -0300

    fix: only a test route for search

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,34 +1,35 @@
-const List = require('../views/lists/list/list.marko')
-const db = require('../../config/database')
-const ItemDao = require('../infra/item-dao')
+// const List = require('../views/lists/list/list.marko')
+// const db = require('../../config/database')
+// const ItemDao = require('../infra/item-dao')
 
 module.exports = (app) => {
     app.get('/', function(req, resp) {
         resp.send('ok, but nothing here :/')
     })
     
-    app.post('/item', function(req, resp) {
-        console.log(req.body)
-        const Item = new ItemDao(db)
-        Item.add(req.body)
-                .then(resp.redirect('/item/list'))   
-                .catch(erro => console.log(erro))
+    app.get('/search/:query', function(req, resp) {
+        console.warn(req.body)
+        resp.send(JSON.stringify({item: req.params.query}))
+        // const Item = new ItemDao(db)
+        // Item.add(req.body)
+        //         .then(resp.redirect('/item/list')) 
+        //         .catch(erro => console.log(erro))
     })
 
-    app.get('/item/list', function(req, resp) {
-        const Item = new ItemDao(db)
+    // app.get('/item/list', function(req, resp) {
+    //     const Item = new ItemDao(db)
 
-        Item.list()
-            .then(items => resp.marko(
-                List,
-                {
-                    items
-                }
-            ))
-            .catch(err => console.log(err))
-    })
+    //     Item.list()
+    //         .then(items => resp.marko(
+    //             List,
+    //             {
+    //                 items
+    //             }
+    //         ))
+    //         .catch(err => console.log(err))
+    // })
 
-    app.get('/item/form', function(req, resp) {
-        resp.marko(require('../views/lists/form/form.marko'))
-    })
+    // app.get('/item/form', function(req, resp) {
+    //     resp.marko(require('../views/lists/form/form.marko'))
+    // })
 }
